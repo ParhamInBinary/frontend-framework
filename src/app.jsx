6d14e0a7c34e5e372b92934995b6180fdda25bb1
ParhamInBinart <parham.berenjian@gmail.com>
2023-02-09 18:54:13 +0100

    foundation

--- src/app.jsx
+++ src/app.jsx
@@ -1,32 +1,25 @@
-import { useState } from 'preact/hooks'
-import preactLogo from './assets/preact.svg'
-import './app.css'
+import './app.css';
+import FrameworkName from './FrameworkName';
 
 export function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://preactjs.com" target="_blank">
-          <img src={preactLogo} class="logo preact" alt="Preact logo" />
-        </a>
-      </div>
-      <h1>Vite + Preact</h1>
-      <div class="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/app.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p class="read-the-docs">
-        Click on the Vite and Preact logos to learn more
-      </p>
+      <header>State of Javascript</header>
+
+      <main>
+        <ul data-cy="chart-header">
+          <li>2016</li>
+          <li>2017</li>
+          <li>2018</li>
+          <li>2019</li>
+          <li>2020</li>
+          <li>2021</li>
+        </ul>
+
+        <FrameworkName />
+
+      </main>
     </>
-  )
+  );
 }
