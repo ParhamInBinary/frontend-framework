3872974e9c15843ea3249bbde8da89ec3d629685
ParhamInBinart <parham.berenjian@gmail.com>
2023-02-12 23:36:02 +0100

    restructure and styled

--- src/Frameworks/Frameworks.jsx
+++ src/Frameworks/Frameworks.jsx
@@ -1,16 +1,12 @@
-import FrameworkLine from "./FrameworkLine/FrameworkLine";
+import FrameworkRow from "./FrameworkLine/FrameworkLine";
 import "./frameworks.css";
 
 export default function Frameworks(props) {
   return (
-    <div className="frameworkList">
+    <tbody>
       {props.frameworks.map((framework) => (
-        <div className="framework" data-cy="chart-row">
-          <h3 style={{ color: framework.color }}>{framework.name}</h3>
-          <FrameworkLine framework={framework} />
-          <h3 style={{ color: framework.color }}>{framework.name}</h3>
-        </div>
+          <FrameworkRow framework={framework} />
       ))}
-    </div>
+    </tbody>
   );
 }
