1db734138833b4d3bd57d96949da10a20b5d941f
ParhamInBinart <parham.berenjian@gmail.com>
2023-02-12 12:36:36 +0100

    added timeline logic

--- src/Frameworks/FrameworkLine/FrameworkLine.jsx
+++ src/Frameworks/FrameworkLine/FrameworkLine.jsx
@@ -1,18 +1,23 @@
-import './frameworkLine.css'
+import "./frameworkLine.css";
 
 export default function FrameworkLine(props) {
-    return (
-        <div className='frameworkLine' style={{borderColor: props.framework.color}} data-cy="row-line">
-
-            <div className='circleContainer'>
-                {props.framework.surveys.map((survey) => (
-
-                    <div className='circleStat' style={{borderColor: props.framework.color}} data-cy="chart-circle">
-                        {survey.retention + "%"}
-                    </div>  
-                ))}
-            </div>
-        </div>
-
-    )
+  return (
+    <div
+      className="frameworkLine"
+      style={{ borderColor: props.framework.color }}
+      data-cy="row-line"
+    >
+      <div className="circleContainer">
+        {props.framework.surveys.map((survey) => (
+          <div
+            className="circleStat"
+            style={{ borderColor: props.framework.color }}
+            data-cy="chart-circle"
+          >
+            {survey.retention + "%"}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
