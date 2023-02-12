acb896027b3809133ffa576c0749d7827404fdbb
ParhamInBinart <parham.berenjian@gmail.com>
2023-02-12 12:36:19 +0100

    addded logic for frameworks

--- src/Frameworks/Frameworks.jsx
+++ src/Frameworks/Frameworks.jsx
@@ -1,19 +1,16 @@
-import FrameworkLine from './FrameworkLine/FrameworkLine'
-import './Frameworks.css'
+import FrameworkLine from "./FrameworkLine/FrameworkLine";
+import "./frameworks.css";
 
 export default function Frameworks(props) {
-    return (
-    
-        <div className='frameworkList'>
-          {props.frameworks.map((framework) => (
-            <div className='framework' data-cy="chart-row">
-
-                <h2 style={{color:framework.color}}>{framework.name}</h2>
-                <FrameworkLine framework={framework}/>
-                <h2 style={{color:framework.color}}>{framework.name}</h2>
-            </div>
-          ))}
+  return (
+    <div className="frameworkList">
+      {props.frameworks.map((framework) => (
+        <div className="framework" data-cy="chart-row">
+          <h3 style={{ color: framework.color }}>{framework.name}</h3>
+          <FrameworkLine framework={framework} />
+          <h3 style={{ color: framework.color }}>{framework.name}</h3>
         </div>
-    
-    )
-  }
+      ))}
+    </div>
+  );
+}
